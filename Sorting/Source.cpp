845bb06cd3c5432190170ac8671169bd495ee1fa
unknown <kucheryaroslav7@gmail.com>
2021-11-18 18:24:35 +0300

    implement bubble sort algorythm

---
+++ Sorting/Source.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void printArr(int* arr,int size);
+int* initArr(int size);
+void bubbleSort(int* arr, int size);
+
+int main() {
+	srand(time(NULL));
+	int size = 10;
+	int* arr = initArr(size);
+	printArr(arr, size);
+	bubbleSort(arr, size);
+	printArr(arr, size);
+}
+
+void printArr(int* arr, int size)
+{
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+ }
+	cout << endl;
+}
+
+int* initArr(int size)
+{
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 50 + 1;
+	}
+	return arr;
+}
+
+void bubbleSort(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
