87653c3bd282cae313227a53a5f50e8c2f834fcc
unknown <kucheryaroslav7@gmail.com>
2021-11-18 19:00:31 +0300

    Implement selection sort algorythm

--- Sorting/Source.cpp
+++ Sorting/Source.cpp
@@ -5,13 +5,15 @@ using namespace std;
 void printArr(int* arr,int size);
 int* initArr(int size);
 void bubbleSort(int* arr, int size);
+void selectionSort(int* arr, int size);
 
 int main() {
 	srand(time(NULL));
 	int size = 10;
 	int* arr = initArr(size);
 	printArr(arr, size);
-	bubbleSort(arr, size);
+	//bubbleSort(arr, size);
+	selectionSort(arr, size);
 	printArr(arr, size);
 }
 
@@ -42,3 +44,21 @@ void bubbleSort(int* arr, int size)
 		}
 	}
 }
+
+void selectionSort(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++) {
+		int tmp = arr[i];
+		int index = i;
+		for (int j = i; j < size; j++) {
+			if (tmp > arr[j]) {
+				tmp = arr[j];
+				index = j;
+			}
+		}
+		swap(arr[i], arr[index]);
+		printArr(arr, size);
+	}
+}
+
+
